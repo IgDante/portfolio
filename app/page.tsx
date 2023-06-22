a1ef05b4ce632ca01c1504b96e392fc63a1e5871
Dante <danteigor424@gmail.com>
2023-06-22 03:39:20 -0300

    hero-section finalizada

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { HeroSection } from "./components/pages/home/hero-section.tsx";
+
 export default async function Home() {
   return (
     <>
-      <h1>Home Page</h1>
+      <HeroSection />
     </>
   )
 }
